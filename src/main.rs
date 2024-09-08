c32d009b95de1bed1a36133f5ff7d16b849b5b3e
DataEnggNerd <solomon.santhosh@gmail.com>
2024-09-08 14:27:25 +0530

    added exception handling

--- src/main.rs
+++ src/main.rs
@@ -1,20 +1,26 @@
 use std::collections::HashMap;
 use reqwest::get;
-use scraper::{Selector, Html};
+use scraper::{Html, Selector};
 
 #[tokio::main]
-async fn main(){
+async fn main() -> Result<(), Box<dyn std::error::Error>>{
     let url = "https://en.wikipedia.org/wiki/Jersey_Act";
-    let body = get(url).await.unwrap().text().await.unwrap();
-    println!("body = {body:?}");
+    let body = get(url).await?.text().await?;
+    // println!("body = {body:?}");
 
     let document = Html::parse_document(&body);
 
-    let anchor_selector = Selector::parse("a").unwrap();
+    let anchor_selector = Selector::parse("a").expect("No anchor tags found");
 
+    let elements: Vec<_> = document.select(&anchor_selector).collect();
+
+    if elements.is_empty() {
+        return Err("No linkes found".into());
+    }
+    
     let mut links: HashMap<String, String> = HashMap::new();
 
-    for element in document.select(&anchor_selector){
+    for element in elements{
         if let Some(href) = element.value().attr("href"){
             if let Some(text) = element.text().next(){
                 links.insert(text.trim().to_string(), href.to_string());
@@ -25,5 +31,7 @@ async fn main(){
     for (text, href) in &links{
         println!("Text -> {}, Href -> {}", text, href);
     }
+
+    Ok(())
 }
 
