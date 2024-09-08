e5f45806f80655682f5e3e6b238475ae33f305e3
DataEnggNerd <solomon.santhosh@gmail.com>
2024-09-08 14:13:26 +0530

    primary version of scraper

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,29 @@
-fn main() {
-    println!("Hello, world!");
+use std::collections::HashMap;
+use reqwest::get;
+use scraper::{Selector, Html};
+
+#[tokio::main]
+async fn main(){
+    let url = "https://en.wikipedia.org/wiki/Jersey_Act";
+    let body = get(url).await.unwrap().text().await.unwrap();
+    println!("body = {body:?}");
+
+    let document = Html::parse_document(&body);
+
+    let anchor_selector = Selector::parse("a").unwrap();
+
+    let mut links: HashMap<String, String> = HashMap::new();
+
+    for element in document.select(&anchor_selector){
+        if let Some(href) = element.value().attr("href"){
+            if let Some(text) = element.text().next(){
+                links.insert(text.trim().to_string(), href.to_string());
+            }
+        }
+    }
+
+    for (text, href) in &links{
+        println!("Text -> {}, Href -> {}", text, href);
+    }
 }
+
